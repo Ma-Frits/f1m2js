c589692e456136b014712549d843d9cacce16072
Ma-Frits <90691243+Ma-Frits@users.noreply.github.com>
2022-02-01 11:44:49 +0100

    Add files via upload

---
+++ F1M2JS/README.md
@@ -0,0 +1,3 @@
+# fase 1 module 2 museum online
+## javescript
+[live view](https://34034.hosts1.ma-cloud.nl/indeex.html)
---
+++ F1M2JS/kat.js
@@ -0,0 +1,7 @@
+const kat = {
+    name: 'luna',
+    age: '1',
+    catcolor: 'blue',
+    image: 'https://cat-care.nl/uploads/images/Headers/header.jpg'
+}
+
